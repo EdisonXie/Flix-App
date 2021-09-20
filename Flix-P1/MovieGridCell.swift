573584653f68781b1d93078dc7c009c7b9fc78fc
EdisonXie <68284121+EdisonXie@users.noreply.github.com>
2021-09-20 18:46:15 -0400

    New Collection View

---
+++ Flix-P1/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix-P1
+//
+//  Created by Edison Xie on 9/20/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
